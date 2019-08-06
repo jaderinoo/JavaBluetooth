455593aef57954c557038b03b4acf1927c516dce
Jad El-Khatib <jaderinoo@gmail.com>
2019-08-06 18:24:58 -0400

    Added description

--- FindMyGlasses/src/Main.java
+++ FindMyGlasses/src/Main.java
@@ -1,3 +1,19 @@
+/*Research
+ * Methods to connect via bluetooth:
+ * http://www.aviyehuda.com/blog/2010/01/08/connecting-to-bluetooth-devices-with-java/
+ * 
+ * Bluetooth Chips for board:
+ * https://www.amazon.com/KEDSUM-Serial-Module-Wireless-Transceiver/dp/B0093XAV4U/ref=sr_1_7?keywords=bluetooth+receiver+chip&qid=1565129671&s=gateway&sr=8-7
+ * 
+ * Create Simple program with visuals that allows users to click a button that will make a small bluetooth speaker emit sounds for 5 - 10 seconds.
+ * 
+ * Use PC and E3 Mini for testing.
+ * 
+ * Breakdown:
+ * When button is clicked - send power to board
+ * When board has power - Play sound
+ */
+
 
 public class Main {
 	//Initial Upload
