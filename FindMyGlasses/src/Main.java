e6bb71b718fbb899de662fc61528fa049b61f294
Jad El-Khatib <jaderinoo@gmail.com>
2019-08-06 18:48:21 -0400

    Fixed Links

--- FindMyGlasses/src/Main.java
+++ FindMyGlasses/src/Main.java
@@ -1,9 +1,10 @@
 /*Research
  * Methods to connect via bluetooth:
  * http://www.aviyehuda.com/blog/2010/01/08/connecting-to-bluetooth-devices-with-java/
+ * https://www.buildcircuit.com/simple-experiment-with-bluetooth-and-arduino/
  * 
  * Bluetooth Chips for board:
- * https://www.amazon.com/KEDSUM-Serial-Module-Wireless-Transceiver/dp/B0093XAV4U/ref=sr_1_7?keywords=bluetooth+receiver+chip&qid=1565129671&s=gateway&sr=8-7
+ * https://www.amazon.com/gp/offer-listing/B01CKW4FSI/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01CKW4FSI&linkCode=am2&tag=webstoreinfo-20&linkId=4feae88d40e9df3bcc17db4cab165dab
  * 
  * Create Simple program with visuals that allows users to click a button that will make a small bluetooth speaker emit sounds for 5 - 10 seconds.
  * 
