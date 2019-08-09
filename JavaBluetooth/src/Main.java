e14358b427955ac911b4df30d171d1da1c44c1bf
Jad El-Khatib <jaderinoo@gmail.com>
2019-08-08 21:54:58 -0400

    Test

---
+++ JavaBluetooth/src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Research
+ * 
+ * Methods to connect via bluetooth:
+ * http://www.aviyehuda.com/blog/2010/01/08/connecting-to-bluetooth-devices-with-java/
+ * https://www.buildcircuit.com/simple-experiment-with-bluetooth-and-arduino/
+ * 
+ * Bluetooth Chips for board:
+ * https://www.amazon.com/gp/offer-listing/B01CKW4FSI/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01CKW4FSI&linkCode=am2&tag=webstoreinfo-20&linkId=4feae88d40e9df3bcc17db4cab165dab
+ * 
+ * Create Simple program with visuals that allows users to click a button that will make a small bluetooth speaker emit sounds for 5 - 10 seconds.
+ * 
+ * Use PC and E3 Mini for testing.
+ * 
+ * Breakdown:
+ * When button is clicked - send power to board
+ * When board has power - Play sound
+ */
+
+public class Main {
+
+}
